9764e706f41ffd1d3a22f7565ea44375adecd639
KR Shanto <krshanto2005@gmail.com>
2026-02-19 15:26:55 +0600

    feat: add divisions list of Bangladesh

---
+++ lib/divisions.ts
@@ -0,0 +1,10 @@
+export const divisions = [
+  { name: "ঢাকা", value: "Dhaka", offset: 0 },
+  { name: "চট্টগ্রাম", value: "Chattogram", offset: -5 },
+  { name: "খুলনা", value: "Khulna", offset: 5 },
+  { name: "রাজশাহী", value: "Rajshahi", offset: 7 },
+  { name: "বরিশাল", value: "Barishal", offset: 1 },
+  { name: "সিলেট", value: "Sylhet", offset: -6 },
+  { name: "রংপুর", value: "Rangpur", offset: 6 },
+  { name: "ময়মনসিংহ", value: "Mymensingh", offset: 0 },
+];
